0efdba73b162f2b66317273f4e41d57830b55dc6
LeandroL18 <leandrolipani6@gmail.com>
2024-02-05 15:37:35 +0100

    feat: stampa dei valori delle proprietà di due oggetti istanziati

--- index.php
+++ index.php
@@ -16,4 +16,18 @@
         
         }
     }
+
+    $movie1 = new Movie("My title 1", "My director 1");
+
+    $movie2 = new Movie("My title 2", "My director 2");
+
+    var_dump($movie1);
+    echo "<br><br>";
+
+    var_dump($movie2);
+    echo "<br><br>";
+
+    echo "Movie1: " . $movie1 -> getInfo() . "<br><br>";
+
+    echo "Movie2: " . $movie2 -> getInfo();
 ?>
